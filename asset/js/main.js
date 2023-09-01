3d0ec21f36c4a8d48bccb9f01e8d249a42b6a5f9
danielplebani <danielpleba03@gmail.com>
2023-09-01 15:29:17 +0200

    compo minato game

---
+++ asset/js/main.js
@@ -0,0 +1,33 @@
+/*
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+*/
+
+const startGame = document.querySelector('button');
+
+startGame.addEventListener('click', function (){
+    console.log("l'utente ha iniziato il gioco");
+
+    const containerMarkup = document.getElementById('container');
+    containerMarkup.className = 'containerMarkup';
+
+
+    startGame.className = 'd-none';
+
+    const grill = document.getElementById('grill');
+    grill.className = 'd-flex flex-wrap cursorPointer';
+
+    for (let i = 1; i <= 100; i++) {
+        const cell = document.createElement('div');
+        cell.className = 'markupColonna'
+
+        grill.append(cell);
+
+        cell.addEventListener('click', function (){
+            cell.classList.toggle('bg-primary');
+            console.log('hai cliccato sulla cella numero: ' + i);
+        })
+    }
+})
